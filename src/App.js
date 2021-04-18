34121a797edd0ced86c7f9ef19c47bf05f091b7e
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-18 15:01:52 +0300

    modifying the home page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>React calculator</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/IBUNHABIBU/react-calculator"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fully functional react calculator app
         </a>
       </header>
     </div>

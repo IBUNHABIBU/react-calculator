e87d8b606b1ed47b37eaf6def48198e205927a9a
IBUNHABIBU <ibunhabib@gmail.com>
2021-04-30 22:01:42 +0300

    Removing the boiler plate

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Big from 'big.js';
 
-const propTypes = {
-  prop: PropTypes,
-};
-
 export default class App extends Component {
   constructor(props) {
     super(props);

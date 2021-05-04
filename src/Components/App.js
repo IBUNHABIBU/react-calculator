d722fb0a7cd49435f6c3f48c2509c08425f926af
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-04 07:47:43 +0000

    Creating the app component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Big from 'big.js';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+const App = () => (
+  <div>
+    <h1>The React calculator</h1>
+    <p>This is the second milestone of the react app</p>
+  </div>
+);
 
-  render() {
-    return (
-      <div>
-        <h1 align="center">React calculator</h1>
-        <p align="center">This is the first milestone of the react calculator project</p>
-      </div>
-    );
-  }
-}
+export default App;

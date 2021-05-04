9cc92cf80f35cf8063e004db308e49d2bb88c9be
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-04 11:42:42 +0000

    adding button panel

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const Button = ({ name }) => (
   <>
     <div>
       { name } 
     </div>
   </>
-)
+);
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+export default Button;
--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 const Display = ({ result }) => (
   <>
     <div>
-      { result } 
+      { result }
     </div>
   </>
 );
 
 Display.defaultProps = {
-  result: 0
-}
+  result: 0,
+};
 
 Display.propTypes = {
-  result: PropTypes.string.isRequired,
-}
+  result: PropTypes.string,
+};
 export default Display;

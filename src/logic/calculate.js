6c21f5b0de3b50fc5ad4a985a44ddb6265e8dac0
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-05 13:25:25 +0000

    Adding calculate methods

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,11 +1,15 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import Big from 'big.js';
 import PropTypes from 'prop-types';
 
-const Calculate = ({ data, btnName }) => (
-  <>
-  </>
-);
+const Calculate = ({ data, btnName }) => {
+  let { total, next, operation } = data;
+  return (
+    <>
+    </>
+  );
+}
 
 Calculate.propTypes = {
   data: PropTypes.string.isRequired,

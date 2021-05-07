a51b9cac6d554f51361bb5b4aea959eeb248c09c
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-07 12:26:57 +0000

    Implementing clickHandler

--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -3,40 +3,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = ({ clickHandler }) => (
-  <>
-    <div>
-      <Button value="AC" />
-      <Button value="+/-" />
-      <Button value="%" />
-      <Button value="÷" />
-    </div>
-    <div>
-      <Button value="7" />
-      <Button value="8" />
-      <Button value="9" />
-      <Button value="X" />
-    </div>
-    <div>
-      <Button value="4" />
-      <Button value="5" />
-      <Button value="6" />
-      <Button value="-" />
-    </div>
-    <div>
-      <Button value="1" />
-      <Button value="2" />
-      <Button value="3" />
-      <Button value="+" />
-    </div>
-    <div>
-      <Button value="0" />
-      <Button value="." />
-      <Button value="=" />
-    </div>
-  </>
-);
+const ButtonPanel = ({ clickHandler }) => {
+  const handleClick = (btnName) => clickHandler(btnName);
 
+  return (
+    <>
+    <div>
+      <Button value="AC" clickHandler />
+      <Button value="+/-" clickHandler />
+      <Button value="%" clickHandler />
+      <Button value="÷" clickHandler />
+    </div>
+    <div>
+      <Button value="7" clickHandler />
+      <Button value="8" clickHandler />
+      <Button value="9" clickHandler />
+      <Button value="X" clickHandler />
+    </div>
+    <div>
+      <Button value="4" clickHandler />
+      <Button value="5" clickHandler />
+      <Button value="6" clickHandler />
+      <Button value="-" clickHandler />
+    </div>
+    <div>
+      <Button value="1" clickHandler />
+      <Button value="2" clickHandler />
+      <Button value="3" clickHandler />
+      <Button value="+" clickHandler />
+    </div>
+    <div>
+      <Button value="0" clickHandler />
+      <Button value="." clickHandler />
+      <Button value="=" clickHandler />
+    </div>
+    </>
+  );
+};
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func.isRequired,
 };

77a1244fdb5e5e335a87c430d1e08751ac87da88
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-17 03:53:20 +0300

    Changint the app from class component to functional component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setnext] = useState(null);
+  const [operation, setoperation] = useState(null);
+  const handleClick = (btnName) => {
+    const data = {
+      total,
+      next,
+      operation,
     };
-  }
+    const modifiedData = calculate(data, btnName);
+    setTotal(modifiedData.total);
+    setnext(modifiedData.next);
+    setoperation(modifiedData.operation);
+  };
+  return (
+    <>
+      <div className="display-container">
+        <Display result={`${total || ''} ${operation || ''} ${next || ''}`} />
+      </div>
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
-  handleClick = (btnName) => this.setState((data) => calculate(data, btnName));
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <>
-        <div className="display-container">
-          <Display result={`${total || ''} ${operation || ''} ${next || ''}`} />
-        </div>
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+export default App;

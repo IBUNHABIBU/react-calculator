027079380433c38d1595cd76abf2bebd064f707a
IBUNHABIBU <ibunhabib@gmail.com>
2021-05-27 18:17:12 +0300

    testing the operations

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -4,13 +4,36 @@ let data;
 beforeEach(() => {
   data = { total: null, next: null, operation: null };
 });
-describe('Calculate ', () => {
-    test('Number concatenation when no operation', () => {
-        it('should concatenate the first items', () => {
-            data = { total: "5", next: null, operation: null}
-            const btnName = "2"
-            const result = calculate(data,btnName);
-            expect(result.total).toEqual(8);
-        })
+
+describe("calculate function when no operation", ()=> {
+    it("concatenates the button value to total when next is null", ()=> {
+        data = { total: "2", next: null, operation: null }
+        let btnValue = "5"
+        let result = calculate(data, btnValue);
+        expect(result.total).toEqual("25");
+    })
+
+    it("concatenates the button value to next ", ()=> {
+        data = { total: "2", next: "48", operation: "+" }
+        let btnValue = "5"
+        let result = calculate(data, btnValue);
+        expect(result.next).toEqual("485");
+    })
+})
+
+describe("calculate function when there is  operation", ()=> {
+    it("adds two numbers when the operation is + ", ()=> {
+        data = { total: "2", next: "8", operation: "+" }
+        let btnValue = "="
+        let result = calculate(data, btnValue);
+        console.log(result);
+        expect(result.total).toEqual("10");
+    })
+
+    it("subtracts two numbers when the operation is -  ", ()=> {
+        data = { total: "2", next: "48", operation: "-" }
+        let btnValue = "="
+        let result = calculate(data, btnValue);
+        expect(result.total).toEqual("-46");
     })
 })

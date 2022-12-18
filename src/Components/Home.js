63b218cef684485213faec1cd626e1dafe54c90c
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-18 10:15:03 +0300

    Remove errors

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
   <div className="home-container">
-    <h1 align="center">Welcome to Math-magician Calculator</h1>
+    <h1>Welcome to Math-magician Calculator</h1>
     <p>
       In this project you will learn how to build a very simple React application.
       You will work in a real-world project for a
